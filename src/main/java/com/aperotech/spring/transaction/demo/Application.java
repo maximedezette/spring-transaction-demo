c9721f7e6606c6d6a25efe15ae475538583970bb
maxime.dezette <m.dezette@gmail.com>
2023-10-07 10:47:32 +0200

    initial commit :tada:

---
+++ src/main/java/com/aperotech/spring/transaction/demo/Application.java
@@ -0,0 +1,13 @@
+package com.aperotech.spring.transaction.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aperotech/spring/transaction/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aperotech.spring.transaction.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
